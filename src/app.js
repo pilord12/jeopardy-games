4b6c51854fb877e4a65bbc5cb789119b08bdf4e8
Keaton Smith <keaton.rob.smith@hpe.com>
2018-02-21 20:47:01 -0500

    Added basic parsing for a game title

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
-import cheerio from 'cheerio';
 import { getGame } from './backend/api/games';
+import { parseGame } from './backend/parsing/gameParser';
 
 // Just get the first game for now so we can test we're doing something
 getGame(1).then((response) => {
-  const $ = cheerio.load(response.data);
-  const firstRound = $('div#jeopardy_round');
-  const firstRoundTable = $('table.round > tbody > tr:nth-child(2) > td.clue:nth-child(1) > table > tbody > tr:nth-child(1) > td > div', firstRound);
-  console.log($(firstRoundTable[0]).attr('onmouseover'));
-  console.log($(firstRoundTable[0]).attr('onmouseout'));
+  console.log(parseGame(response.data));
 });
---
+++ src/backend/parsing/gameParser.js
@@ -0,0 +1,41 @@
+import cheerio from 'cheerio';
+
+// Regex to match the title of a show
+const TITLE_STRING_REGEX = /Show #(\d+) - (.*)/;
+
+// Selector constants for parsing HTML
+const BASE_CONTENT = 'div#content';
+const TITLE_HEADER = BASE_CONTENT + ' > div#game_title > h1';
+
+export const parseGame = (gameHtml) => {
+  const $ = cheerio.load(gameHtml);
+  const titleString = $(TITLE_HEADER).text();
+
+  const titleObj = parseTitleString(titleString);
+
+  return {
+    ...titleObj,
+  };
+};
+
+/**
+ * Parses a title string of the titleStringRegex format into a show number and date
+ * @param titleString the string to match against
+ * @return an object of the form { showNumber, showDate }
+ */
+const parseTitleString = (titleString) => {
+  const matches = titleString.match(TITLE_STRING_REGEX);
+
+  if (matches.length <= 2) {
+    console.warn('Failed title string match for ' + titleString);
+    return {};
+  }
+
+  const showNumber = matches[1];
+  const showDate = Date.parse(matches[2]);
+
+  return {
+    showNumber,
+    showDate,
+  };
+};
